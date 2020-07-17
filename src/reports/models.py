84eabfe9cbdf04fba7a293cac28c374e98371847
saber soliman <sm.soliman@hotmail.com>
2020-07-17 09:42:19 +0200

    models files update

--- src/reports/models.py
+++ src/reports/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.urls import reverse
 from account.models import Discipline ,Account
 from django.utils import timezone
-
+import datetime
+import os
 
 shiftchoices=(("Day","Day"),("Night","Night"))
 
@@ -32,10 +33,31 @@ class Area(models.Model):
     def __str__(self):
         return self.area
 
+def set_filename_format(now,instance, filename):
+    
+    return "{username}-{date}-{microsecond}{extension}".format(
+        username=instance.created_by,
+        date=str(now.date()),
+        microsecond=now.microsecond,
+        extension=os.path.splitext(filename)[1],
+    )        
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    now = datetime.datetime.now()
+    path = "daily/{year}/{month}/{day}/{username}/{filename}".format(
+        year=now.year,
+        month=now.month,
+        day=now.day,
+        username=instance.created_by,
+        filename=set_filename_format(now,instance, filename),
+    )
+   
+
+    return path
 
 class DailyReport(models.Model):
-    
+
     created_by=models.ForeignKey(Account,related_name='created_by',related_query_name='created_by' ,on_delete=models.PROTECT)
     wind_velocity=models.ForeignKey(Wind, blank=True ,null=True,on_delete=models.SET_NULL)
     temp=models.CharField(blank=True, max_length=10)
@@ -44,7 +66,7 @@ class DailyReport(models.Model):
     shift=models.CharField(max_length=20,blank=True ,null=True ,choices=shiftchoices ,default='Day')
     updated=models.DateTimeField(auto_now_add=False , auto_now=True)
     status=models.BooleanField(default=True)
-    attachment=models.ImageField(upload_to="daily/files/",null=True ,blank=True)
+    attachment=models.FileField(upload_to=user_directory_path,null=True ,blank=True)
 
     def __str__(self):
         return str(self.id)
